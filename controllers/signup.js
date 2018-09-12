4043d27cdf524a3440588c6b0599094bebae4838
Igor Chernyshev <igorchern07@gmail.com>
2018-09-12 11:39:30 -0500

    MVC starter

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,28 +1,11 @@
 var express = require('express');
 var orm = require('../config/orm');
 var router = express.Router();
-var Joi = require('joi');
-
-var Student = require('../models/student');
-var schema = require('../controllers/joiSchema');
 
 router.get('/', function(req, res) {
   res.render('signup', {
     urlPath: req.baseUrl
   });
 });
-
-// student.selectAll('students', function(result) {
-//   console.log(result);
-// });
-
-// Student.insertOne(
-//   'students',
-//   ['first_name', 'last_name', 'email', 'password'],
-//   ['John', 'Doe', 'jdoe@email.com', 'asdfasdf'],
-//   function(result) {
-//     console.log(result);
-//   }
-// );
 
 module.exports = router;
