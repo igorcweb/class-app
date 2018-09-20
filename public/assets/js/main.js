d6728aad2de4dba7c28ca9fcd0070cac16b284bd
Igor Chernyshev <igorchern07@gmail.com>
2018-09-19 23:38:46 -0500

    select button disabled for filled classes, alert message

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,129 +1,148 @@
-//Search Filter
-var filter = $('#filter');
-var lis = $('li.class-name');
-$(document).on('keyup', '#filter', filterClasses);
-function filterClasses() {
-  var filterValue = filter.val().toLowerCase();
-  $.each(lis, function(index, li) {
-    var className = $(li)
-      .text()
-      .toLowerCase();
-    if (className.includes(filterValue)) {
-      $(li)
-        .addClass('d-block')
-        .removeClass('d-none');
-    } else {
-      $(li)
-        .addClass('d-none')
-        .removeClass('d-block');
-    }
-  });
-}
-
-//Toggle Class Description
-var className = $('li.class-name');
-var classes = $('ul.classes');
-className.on('click', function(e) {
-  e.stopPropagation();
-  var $this = $(this);
-  $this.toggleClass('is-open');
-  $this.next().toggleClass('is-open');
-});
-
-//Select Button
-if (!selectedIds) {
-  var selectedIds = [];
-}
-var cart = $('.cart');
-var addedClasses = $('.addedClasses');
-$.get('/api/classes', function(data) {
-  classes.on('click', '.select', function(e) {
-    e.stopPropagation();
-    // Clearing out the input
-    filter.val('');
-    // Displayin all classes
+(function($) {
+  //Search Filter
+  var filter = $('#filter');
+  var lis = $('li.class-name');
+  $(document).on('keyup', '#filter', filterClasses);
+  function filterClasses() {
+    var filterValue = filter.val().toLowerCase();
     $.each(lis, function(index, li) {
-      $(li).addClass('d-block');
-    });
-    var $this = $(this);
-    //closing description
-    $this.closest('li').removeClass('is-open');
-    //rotating carret back
-    $this
-      .closest('li')
-      .prev()
-      .removeClass('is-open');
-    //Geting variables from data attributes (destructuring)
-    var {
-      classId,
-      className,
-      classCode,
-      classSemester,
-      tuition
-    } = this.dataset;
-    //Creatign addedClass object
-    var addedClass = { classId, className, classCode, classSemester, tuition };
-
-    $this.addClass('selected');
-    cart.addClass('show');
-    // Displays Class Name in Card When Selected
-    if ($this.hasClass('selected')) {
-      selectedIds.push(classId);
-      //removing duplicate values
-      selectedIds = Array.from(new Set(selectedIds));
-    } else if ($this.data('class-name') === className) {
-      //removing id from array
-      var indexToRemove = selectedIds.indexOf(classId);
-      selectedIds.splice(indexToRemove, 1);
-    }
-    addedClasses.empty();
-    $.each(data, function(index, value) {
-      if (selectedIds.includes(value.id.toString())) {
-        var classTitle =
-          '<li class="mb-2 added-class">' +
-          value.name +
-          `<i class="fas fa-times" data-class-id="${classId}"></i>` +
-          '</li>' +
-          '<hr>';
-        addedClasses.append(classTitle);
+      var className = $(li)
+        .text()
+        .toLowerCase();
+      if (className.includes(filterValue)) {
+        $(li)
+          .addClass('d-block')
+          .removeClass('d-none');
+      } else {
+        $(li)
+          .addClass('d-none')
+          .removeClass('d-block');
       }
     });
-    //Hide cart if it is empty
-    if (!selectedIds.length) {
-      cart.removeClass('show');
+  }
+
+  //Toggle Class Description
+  var className = $('li.class-name');
+  var classes = $('ul.classes');
+  className.on('click', function(e) {
+    e.stopPropagation();
+    var $this = $(this);
+    $this.toggleClass('is-open');
+    $this.next().toggleClass('is-open');
+  });
+
+  //Select Button
+  if (!selectedIds) {
+    var selectedIds = [];
+  }
+  //Disabled select button if no available spaces
+  var select = $('.select');
+  $.each(select, function(index, selectButton) {
+    if ($(selectButton).data('available-spaces') === 0) {
+      $(selectButton).attr('disabled', 'true');
+      $(selectButton)
+        .prev()
+        .removeClass('d-none');
     }
   });
-  cart.on('click', '.fa-times', function(e) {
-    e.stopPropagation();
-    var className = $(this)
-      .closest('li')
-      .text();
+  var cart = $('.cart');
+  var addedClasses = $('.addedClasses');
 
-    $.each(data, function(index, obj) {
-      if (className === obj.name) {
-        var indexToRemove = selectedIds.indexOf(obj.id.toString());
+  $.get('/api/classes', function(data) {
+    classes.on('click', '.select', function(e) {
+      e.stopPropagation();
+      var $this = $(this);
+      // Clearing out the input
+      filter.val('');
+      // Displayin all classes
+      $.each(lis, function(index, li) {
+        $(li).addClass('d-block');
+      });
+      //closing description
+      $this.closest('li').removeClass('is-open');
+      //rotating carret back
+      $this
+        .closest('li')
+        .prev()
+        .removeClass('is-open');
+      //Geting variables from data attributes (destructuring)
+      var {
+        classId,
+        className,
+        classCode,
+        classSemester,
+        tuition
+      } = this.dataset;
+      //Creatign addedClass object
+      var addedClass = {
+        classId,
+        className,
+        classCode,
+        classSemester,
+        tuition
+      };
+
+      $this.addClass('selected');
+      cart.addClass('show');
+      // Displays Class Name in Card When Selected
+      if ($this.hasClass('selected')) {
+        selectedIds.push(classId);
+        //removing duplicate values
+        selectedIds = Array.from(new Set(selectedIds));
+      } else if ($this.data('class-name') === className) {
+        //removing id from array
+        var indexToRemove = selectedIds.indexOf(classId);
         selectedIds.splice(indexToRemove, 1);
       }
-    });
-    addedClasses.empty();
-    $.each(data, function(index, value) {
-      if (selectedIds.includes(value.id.toString())) {
-        var classTitle =
-          '<li class="mb-2 added-class">' +
-          value.name +
-          `<i class="fas fa-times"></i>` +
-          '</li>' +
-          '<hr>';
-        addedClasses.append(classTitle);
-      }
+      addedClasses.empty();
+      $.each(data, function(index, value) {
+        if (selectedIds.includes(value.id.toString())) {
+          var classTitle =
+            '<li class="mb-2 added-class">' +
+            value.name +
+            `<i class="fas fa-times" data-class-id="${classId}"></i>` +
+            '</li>' +
+            '<hr>';
+          addedClasses.append(classTitle);
+        }
+      });
+      //Hide cart if it is empty
       if (!selectedIds.length) {
-        //Hide cart if it is empty
         cart.removeClass('show');
       }
     });
+    cart.on('click', '.fa-times', function(e) {
+      e.stopPropagation();
+      var className = $(this)
+        .closest('li')
+        .text();
+
+      $.each(data, function(index, obj) {
+        if (className === obj.name) {
+          var indexToRemove = selectedIds.indexOf(obj.id.toString());
+          selectedIds.splice(indexToRemove, 1);
+        }
+      });
+      addedClasses.empty();
+      $.each(data, function(index, value) {
+        if (selectedIds.includes(value.id.toString())) {
+          var classTitle =
+            '<li class="mb-2 added-class">' +
+            value.name +
+            `<i class="fas fa-times"></i>` +
+            '</li>' +
+            '<hr>';
+          addedClasses.append(classTitle);
+        }
+        if (!selectedIds.length) {
+          //Hide cart if it is empty
+          cart.removeClass('show');
+        }
+      });
+    });
+    // Hide cart on outside click
+    $(document).on('click', function(e) {
+      cart.removeClass('show');
+    });
   });
-  // Hide cart on outside click
-  $(document).on('click', function(e) {
-    cart.removeClass('show');
-  });
-});
+})(jQuery);
