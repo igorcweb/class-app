caf85dc9c8fa7b76bc6bb3b734213b088a99ea4e
Igor Chernyshev <igorchern07@gmail.com>
2018-09-28 14:11:15 -0500

    refactor

--- controllers/index.js
+++ controllers/index.js
@@ -4,14 +4,13 @@ var ensureAuthenticated = require('../helpers/authMiddleware')
   .ensureAuthenticated;
 
 router.get('/', ensureAuthenticated, function(req, res) {
-  var { id, first_name, last_name, registeredIds, droppedIds } = req.user[0];
+  var { id, first_name, last_name, registeredIds } = req.user[0];
   res.render('index', {
     success: req.flash('success'),
     id,
     first_name,
     last_name,
-    registeredIds,
-    droppedIds
+    registeredIds
   });
 });
 
